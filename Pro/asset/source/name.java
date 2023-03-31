acfdd3b05663aab2740070d8de5cf4a3d43c97e7
vardhan2011 <109521885+vardhan2011@users.noreply.github.com>
2023-03-31 12:03:15 +0530

    Add files via upload

---
+++ Pro/asset/source/name.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class name {
+	
+	public static void main(String[] args) {
+		
+			
+	 Scanner sc=new Scanner (System.in);
+	 System.out.println("enter name");
+	 
+	
+		        
+		        System.out.println("the name is =="+sc);
+		        
+		   
+		    }
+		}
+
+	
+
